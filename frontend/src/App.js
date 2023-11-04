5a120487265353da832f8347aac7f65f98467090
ypragna <ypragna21@gmail.com>
2023-11-04 12:43:22 +0530

    Product details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
+
 function App() {
   return (
-    <div>
-      <h1>Welcome to Proshop</h1>
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
 
