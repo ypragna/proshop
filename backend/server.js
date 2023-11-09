0e1cf313c91deff020cfa371aae5e1bd597ae532
ypragna <ypragna21@gmail.com>
2023-11-09 20:53:32 +0530

    User Authentication and Register

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
 import connectDB from "./config/db.js";
 import router from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const port = process.env.PORT || 5000;
 connectDB();
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
+app.use("/api/products", router);
+app.use("/api/users", userRoutes);
 app.get("/", (req, res) => {
   res.send("API is running");
 });
-app.use("/api/products", router);
 app.use(notFound);
 app.listen(port, () => console.log(`Server is running in the port ${port}`));
